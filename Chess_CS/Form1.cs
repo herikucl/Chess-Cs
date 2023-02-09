48b41511576b08b770e745b2cb7c7136e1c4fb35
herikucl <79945400+herikucl@users.noreply.github.com>
2023-02-09 16:59:05 -0300

    Adicionar arquivos de projeto.

---
+++ Chess_CS/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Chess_CS
+{
+    public partial class Form1 : Form
+    {
+        Game game;
+        Point PosA = new Point(50, 50);
+        Point PosB = new Point(50, 50);
+
+        public Form1()
+        {
+            InitializeComponent();
+            game = new Game();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+
+            SolidBrush brush = new SolidBrush(Color.LightGray);
+            Font ft = new Font("Arial", 10);
+
+
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    g.FillRectangle(brush, i * 100, j * 100, 100, 100);
+                    if (game.Pos[i, j] != null)
+                    {
+                        g.DrawImage(game.Pos[i, j].img, i * 100, j * 100, 100, 100);
+                    }
+                    if (brush.Color == Color.LightGray)
+                    {
+                        brush.Color = Color.Black;
+                    }
+                    else
+                    {
+                        brush.Color = Color.LightGray;
+                    }
+
+                }
+                if (brush.Color == Color.LightGray)
+                {
+                    brush.Color = Color.Black;
+                }
+                else
+                {
+                    brush.Color = Color.LightGray;
+                }
+            }
+
+            if (PosA.X != 50)
+            {
+                g.DrawRectangle(Pens.Red, PosA.X * 100, PosA.Y * 100, 100, 100);
+                g.DrawRectangle(Pens.Yellow, PosB.X * 100, PosB.Y * 100, 100, 100);
+            }
+
+            if (game.BlackTurn)
+            {
+                g.DrawString("Turn: Black", ft, Brushes.Red, 0, 0);
+            }
+            else
+            {
+                g.DrawString("Turn: White", ft, Brushes.Red, 0, 0);
+            }
+
+            Font gameover = new Font("Arial", 30);
+
+            if (game.gameOver)
+            {
+                StringFormat sf = new StringFormat();
+                sf.Alignment = StringAlignment.Center;
+                RectangleF r = new RectangleF(0, 0, Width, Height);
+
+                if (game.BlackTurn)
+                {
+                    g.DrawString("White Win", gameover, Brushes.Red, r, sf);
+                }
+                else
+                {
+                    g.DrawString("Black Win", gameover, Brushes.Red, r, sf);
+                }
+            }
+
+
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!game.gameOver)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (((e.Location.X > i * 100) && (e.Location.X < (i + 1) * 100)) && ((e.Location.Y > j * 100) && (e.Location.Y < (j + 1) * 100)))
+                        {
+                            PosA.X = i;
+                            PosA.Y = j;
+                        }
+                    }
+                }
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!game.gameOver)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (((e.Location.X > i * 100) && (e.Location.X < (i + 1) * 100)) && ((e.Location.Y > j * 100) && (e.Location.Y < (j + 1) * 100)))
+                        {
+                            PosB.X = i;
+                            PosB.Y = j;
+                            if (game.Pos[PosA.X, PosA.Y] != null)
+                            {
+                                if (((game.Pos[PosA.X, PosA.Y].black) && (game.BlackTurn)) || ((!game.Pos[PosA.X, PosA.Y].black) && (!game.BlackTurn)))
+                                {
+                                    game.Move(PosA, PosB);
+                                }
+                            }
+                        }
+                    }
+                }
+                PosA = PosB = new Point(50, 50);
+                Invalidate();
+            }
+        }
+    }
+}
---
+++ Chess_CS/Game.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Drawing;
+
+namespace Chess_CS
+{
+    internal class Game
+    {
+        public Piece[,] Pos = new Piece[8, 8];
+        public bool BlackTurn = false;
+        public bool gameOver = false;
+        public Game()
+        {
+            Pos[0, 0] = new Rook(0, 0, true);
+            Pos[1, 0] = new Knight(1, 0, true);
+            Pos[2, 0] = new Bishop(2, 0, true);
+            Pos[3, 0] = new King(3, 0, true);
+            Pos[4, 0] = new Queen(4, 0, true);
+            Pos[5, 0] = new Bishop(5, 0, true);
+            Pos[6, 0] = new Knight(6, 0, true);
+            Pos[7, 0] = new Rook(7, 0, true);
+            for (int i = 0; i < 8; i++)
+            {
+                Pos[i, 1] = new Pawn(i, 1, true);
+                Pos[i, 6] = new Pawn(i, 6, false);
+            }
+            Pos[0, 7] = new Rook(0, 7, false);
+            Pos[1, 7] = new Knight(1, 7, false);
+            Pos[2, 7] = new Bishop(2, 7, false);
+            Pos[3, 7] = new King(3, 7, false);
+            Pos[4, 7] = new Queen(4, 7, false);
+            Pos[5, 7] = new Bishop(5, 7, false);
+            Pos[6, 7] = new Knight(6, 7, false);
+            Pos[7, 7] = new Rook(7, 7, false);
+        }
+
+        public void Move(Point a, Point b)
+        {
+            bool CannotMove = false;
+            if (Pos[a.X, a.Y] != null)
+            {
+
+                if ((((b.Y != a.Y) || (b.X != a.X)) && Pos[a.X, a.Y].Move(new Point(b.X, b.Y))) && !((Pos[a.X, a.Y].GetType().Name == "Pawn") && (Pos[b.X, b.Y] != null)))
+                {
+
+                    if (Pos[a.X, a.Y].PosX - b.X == 0)
+                    {
+                        if (Pos[a.X, a.Y].PosY - b.Y > 0)
+                        {
+                            for (int i = 1; i < Pos[a.X, a.Y].PosY - b.Y; i++)
+                            {
+                                if (Pos[a.X, a.Y - i] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 1; i < Math.Abs(Pos[a.X, a.Y].PosY - b.Y); i++)
+                            {
+                                if (Pos[b.X, b.Y - i] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                    }
+                    else if (Pos[a.X, a.Y].PosY - b.Y == 0)
+                    {
+                        if (Pos[a.X, a.Y].PosX - b.X > 0)
+                        {
+                            for (int i = 1; i < Pos[a.X, a.Y].PosX - b.X; i++)
+                            {
+                                if (Pos[a.X - i, a.Y] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 1; i < Math.Abs(Pos[a.X, a.Y].PosX - b.X); i++)
+                            {
+                                if (Pos[b.X - i, b.Y] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                    }
+                    else if (Math.Abs(Pos[a.X, a.Y].PosY - b.Y) == Math.Abs(Pos[a.X, a.Y].PosX - b.X))
+                    {
+
+                        if ((Pos[a.X, a.Y].PosY - b.Y > 0) && (Pos[a.X, a.Y].PosX - b.X > 0))
+                        {
+                            for (int i = 1; i < Math.Abs(Pos[a.X, a.Y].PosX - b.X); i++)
+                            {
+                                if (Pos[b.X + i, b.Y + i] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                        else if ((Pos[a.X, a.Y].PosY - b.Y > 0) && (Pos[a.X, a.Y].PosX - b.X < 0))
+                        {
+                            for (int i = 1; i < Math.Abs(Pos[a.X, a.Y].PosX - b.X); i++)
+                            {
+                                if (Pos[b.X - i, b.Y + i] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                        else if ((Pos[a.X, a.Y].PosY - b.Y < 0) && (Pos[a.X, a.Y].PosX - b.X > 0))
+                        {
+                            for (int i = 1; i < Math.Abs(Pos[a.X, a.Y].PosX - b.X); i++)
+                            {
+                                if (Pos[b.X + i, b.Y - i] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 1; i < Math.Abs(Pos[a.X, a.Y].PosX - b.X); i++)
+                            {
+                                if (Pos[b.X - i, b.Y - i] != null)
+                                {
+                                    CannotMove = true;
+                                }
+                            }
+                        }
+                    }
+                    if (!CannotMove)
+                    {
+                        if (Pos[b.X, b.Y] != null)
+                        {
+                            if (((Pos[b.X, b.Y].black) && (Pos[a.X, a.Y].black)) || (!(Pos[b.X, b.Y].black) && !(Pos[a.X, a.Y].black))) { }
+                            else
+                            {
+
+                                Pos[b.X, b.Y] = Pos[a.X, a.Y];
+                                Pos[a.X, a.Y] = null;
+                                Pos[b.X, b.Y].PosX = b.X;
+                                Pos[b.X, b.Y].PosY = b.Y;
+                                BlackTurn = !BlackTurn;
+
+                            }
+                        }
+                        else
+                        {
+
+                            Pos[b.X, b.Y] = Pos[a.X, a.Y];
+                            Pos[a.X, a.Y] = null;
+                            Pos[b.X, b.Y].PosX = b.X;
+                            Pos[b.X, b.Y].PosY = b.Y;
+                            BlackTurn = !BlackTurn;
+                        }
+                    }
+                }
+                else if ((!Pos[a.X, a.Y].Move(new Point(b.X, b.Y))) && (Pos[b.X, b.Y] != null))
+                {
+                    if (Pos[a.X, a.Y].GetType().Name == "Pawn")
+                    {
+                        if (Pos[a.X, a.Y].Kill(new Point(b.X, b.Y)))
+                        {
+                            Pos[b.X, b.Y] = Pos[a.X, a.Y];
+                            Pos[a.X, a.Y] = null;
+                            Pos[b.X, b.Y].PosX = b.X;
+                            Pos[b.X, b.Y].PosY = b.Y;
+                            BlackTurn = !BlackTurn;
+                        }
+                    }
+                }
+            }
+
+            bool WKing = false;
+            bool BKing = false;
+            foreach (Piece falseKing in Pos)
+            {
+                if (falseKing != null)
+                {
+                    if (falseKing.GetType().Name == "King")
+                    {
+                        if (falseKing.name == "WKing")
+                        {
+                            WKing = true;
+                        }
+                        else
+                        {
+                            BKing = true;
+                        }
+                    }
+                }
+            }
+
+            if ((!WKing) || (!BKing))
+            {
+                gameOver = true;
+            }
+
+        }
+    }
+}
